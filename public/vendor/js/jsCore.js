6cef1211eade3b419c4340b5eac619f7748f2172
Tzolkin <fzavaladev@gmail.com>
2015-09-24 20:47:44 -0500

    Changed landing page

--- public/vendor/js/jsCore.js
+++ public/vendor/js/jsCore.js
@@ -1,14 +1,185 @@
 /// <reference path="jquery-1.11.1.min.js" />
+/// <reference path="jquery.nicescroll.js" />
 /// <reference path="bootstrap.min.js" />
+
+
 $(document).ready(function () {
+
   'use strict';
+
   if ($(window).width() <= 600) {
-    $('.bg-img').html('<img src="img/mudanza.jpg" alt="Background Image" />')
+    $('.bg-img').html('<img src="/vendor/img/mudanza.jpg" alt="Background Image" />')
+
+    $('#collapseTwo').addClass('in');
+    $('#collapseThree').addClass('in');
   }
+
+  $('body').niceScroll();
+
+});
+//Loadlogo
+$(function () {
+  // We create an interval which will be removed when progress completes
+  $('#excelsior').click(function () {
+    $('#excelsior').loadgo();
+    mainDemo();
+    setTimeout(function () {
+      window.open('https://www.youtube.com/watch?v=28LUVpbhJCo', '_blank')
+    }, 3000);
+  });
+
+  var logoInterval;
+  function mainDemo() {
+    var p = 0;
+    $('#excelsior').loadgo('resetprogress');   // reset progress to start again
+    logoInterval = window.setInterval(function () {
+      if ($('#excelsior').loadgo('getprogress') == 100) {
+        // When progress completes, we have to clear the interval
+        window.clearInterval(logoInterval);
+      }
+      else {
+        var prog = p * 10;
+        $('#excelsior').loadgo('setprogress', prog);
+        p++;
+      }
+    }, 150);
+  };
 });
 
+$(function () {
+  // We create an interval which will be removed when progress completes
+  $('#financiero').click(function () {
+    $(this).loadgo();
+    mainDemo();
+    setTimeout(function () {
+      window.open('http://www.elfinanciero.com.mx/universidades/app-para-contratar-mudanzas-lleva-mexicanos-a-silicon-valley.html', '_blank')
+    }, 3000);
+  });
+
+  var logoInterval;
+  function mainDemo() {
+    var p = 0;
+    $('#financiero').loadgo('resetprogress');   // reset progress to start again
+    logoInterval = window.setInterval(function () {
+      if ($('#financiero').loadgo('getprogress') == 100) {
+        // When progress completes, we have to clear the interval
+        window.clearInterval(logoInterval);
+      }
+      else {
+        var prog = p * 10;
+        $('#financiero').loadgo('setprogress', prog);
+        p++;
+      }
+    }, 150);
+  };
+});
 
 $(function () {
+  // We create an interval which will be removed when progress completes
+  $('#angelhack').click(function () {
+    $(this).loadgo();
+    mainDemo();
+    setTimeout(function () {
+      window.open('http://angelhack.com/ah8-meets-mexico-city/', '_blank')
+    }, 3000);
+  });
+
+  var logoInterval;
+  function mainDemo() {
+    var p = 0;
+    $('#angelhack').loadgo('resetprogress');   // reset progress to start again
+    logoInterval = window.setInterval(function () {
+      if ($('#angelhack').loadgo('getprogress') == 100) {
+        // When progress completes, we have to clear the interval
+        window.clearInterval(logoInterval);
+      }
+      else {
+        var prog = p * 10;
+        $('#angelhack').loadgo('setprogress', prog);
+        p++;
+      }
+    }, 150);
+  };
+});
+
+$(function () {
+  // We create an interval which will be removed when progress completes
+  $('#mastercard').click(function () {
+    $(this).loadgo();
+    mainDemo();
+    setTimeout(function () {
+      window.open('https://developer.mastercard.com/portal/display/blogs/2015/08/02/Meet+the+Developers+-+Mexico+City', '_blank')
+    }, 3000);
+  });
+
+  var logoInterval;
+  function mainDemo() {
+    var p = 0;
+    $('#mastercard').loadgo('resetprogress');   // reset progress to start again
+    logoInterval = window.setInterval(function () {
+      if ($('#mastercard').loadgo('getprogress') == 100) {
+        // When progress completes, we have to clear the interval
+        window.clearInterval(logoInterval);
+      }
+      else {
+        var prog = p * 10;
+        $('#mastercard').loadgo('setprogress', prog);
+        p++;
+      }
+    }, 150);
+  };
+});
+
+//Tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+//Animates scroll
+$(function () {
+  $('#conocenos').click(function () {
+    $('.container.content').animatescroll();
+  });
+
+  $('#pasos').click(function () {
+    $('#cd-main-content').animatescroll();
+  });
+});
+
+$(function () {
+  $('.btn-cotizar').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
+      window.top.location.href = 'services'
+    }, 1000);
+  });
+
+  $('#yolo').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
+      window.top.location.href = 'price.html'
+    }, 1000);
+  });
+  $('#tailored').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
+      window.top.location.href = 'user_data_tailored.html'
+    }, 1000);
+  });
+
+  $('.btn-registrar').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
+      window.top.location.href = 'price.html'
+    }, 1000);
+  });
+  $('.user-log').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
+      window.top.location.href = 'admin.html'
+    }, 1000);
+  });
+
   $('.btn-rapida').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
@@ -26,7 +197,7 @@ $(function () {
   $('.btn-stands').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = 'partner/sign_up'
+      window.top.location.href = 'registry_partner.html'
     }, 1000);
   });
 
@@ -47,27 +218,27 @@ $(function () {
   $('.menu-right.two').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = 'partner/sign_up'
+      window.top.location.href = 'registry_partner.html'
     }, 1000);
   });
 
   $('#back').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = '/'
+      window.top.location.href = 'index.html'
     }, 1000);
   });
 
   $('.btn-login-partner').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = 'partner/sign_in'
+      window.top.location.href = 'login_partner.html'
     }, 1000);
   });
   $('.btn-login-user').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = 'customer/sign_in'
+      window.top.location.href = 'login_user.html'
     }, 1000);
   });
 
@@ -99,7 +270,7 @@ $(function () {
   $('.btn-admin').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = 'admin.html'
+      window.top.location.href = 'test.html'
     }, 1000);
   });
   $('.btn-proveedor').click(function () {
@@ -131,14 +302,27 @@ $(function () {
   $('.btn-table').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
+      window.top.location.href = 'user_data_yolo.html'
+    }, 1000);
+  });
+  $('#send').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
       window.top.location.href = 'confirm_reservation.html'
+    }, 1000);
+  });
+  $('#send2').click(function () {
+    $('body').fadeOut('slow');
+    setTimeout(function () {
+      window.top.location.href = 'thanks.html'
     }, 1000);
   });
 
   $('.btn-enterado').click(function () {
     $('body').fadeOut('slow');
     setTimeout(function () {
-      window.top.location.href = 'index.html'
+      window.top.location.href = '/'
     }, 1000);
   });
+
 });
