65fdf1cf0415081d670cf3a470f31f952c6b44a4
Tzolkin <fzavaladev@gmail.com>
2015-09-26 16:26:27 -0500

    Stable version

--- public/vendor/js/btrips.maps.js
+++ public/vendor/js/btrips.maps.js
@@ -1,36 +1,33 @@
+var origin_geometry, destination_geometry;
 $(function() {
   var directionsDisplay = new google.maps.DirectionsRenderer();
   var directionsService = new google.maps.DirectionsService();
-  var handler = Gmaps.build('Google');
+  var btripMap = Gmaps.build('Google');
 
-  handler.buildMap({
+  btripMap.buildMap({
     internal: {
       id: 'map-canvas'
     }
   },
   function() {
-    //directionsDisplay.setMap(handler.getMap());
+    directionsDisplay.setMap(btripMap.getMap());
     if(navigator.geolocation)
       navigator.geolocation.getCurrentPosition(displayOnMap);
   });
 
   function displayOnMap(position){
-    var marker = handler.addMarker({
+    var marker = btripMap.addMarker({
       lat: position.coords.latitude,
       lng: position.coords.longitude
     });
-    handler.map.centerOn(marker);
+    btripMap.map.centerOn(marker);
   };
 
-  function calcRoute(origin, destination) {
-    var origin      = new google.maps.LatLng(19.3894289, -99.1956463);
-    var destination = new google.maps.LatLng(19.3719848, -99.1578443);
+  function calcRoute() {
+    var origin      = new google.maps.LatLng(origin_geometry.location.H, origin_geometry.location.L);
+    var destination = new google.maps.LatLng(destination_geometry.location.H, destination_geometry.location.L);
 
-    // if(navigator.geolocation) {
-    //   navigator.geolocation.getCurrentPosition(function(position) {
-    //     origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    //   });
-    // }
+    btripMap.map.replaceMarkers('');
 
     var request = {
       origin:      origin,
@@ -38,15 +35,32 @@ $(function() {
       travelMode:  google.maps.TravelMode.DRIVING
     };
     directionsService.route(request, function(response, status) {
+      console.log(response);
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(response);
       }
     });
   }
 
-  // calcRoute();
-
   // Completer Section
-  $("#origin").geocomplete();
-  $("#destination").geocomplete();
+  $("#yolo_origin_address")
+    .geocomplete()
+    .bind("geocode:result", function(event, result) {
+      origin_geometry = result.geometry;
+      validateGeometries();
+      // console.log(result);
+    });
+  $("#yolo_destination_address")
+    .geocomplete()
+    .bind("geocode:result", function(event, result) {
+      destination_geometry = result.geometry;
+      validateGeometries();
+      // console.log(result);
+    });
+
+  function validateGeometries() {
+    if ((origin_geometry !== undefined) && (destination_geometry !== undefined)) {
+      calcRoute();
+    }
+  }
 });
