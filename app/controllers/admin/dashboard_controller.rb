2c633673767d62f8a99a244fa5f354991aa7129d
Tzolkin <fzavaladev@gmail.com>
2015-10-22 02:55:25 -0500

    Added admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Admin
   class DashboardController < BaseController
+    layout 'admin'
+
     def home
+      @tailoreds = Tailored.all
+      @yolos = Yolo.all
     end
   end
 end
--- app/models/customer_info.rb
+++ app/models/customer_info.rb
@@ -3,4 +3,8 @@ class CustomerInfo < ActiveRecord::Base
   has_many :yolos
 
   validates_presence_of :name, :paternal_name, :maternal_name, :email, :phone_number
+
+  def full_name
+    "%s %s %s" % [name, paternal_name, maternal_name]
+  end
 end
